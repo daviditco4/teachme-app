9c90ace5570cf2937e9bf375d7b804c11b7267db
Matias <mdaneri@itba.edu.ar>
2022-05-01 16:04:11 -0300

    Se creó la página de carga. Además, se modularizó un widget para tener una consistencia de botones.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:teachme_app/pages/loading_page.dart';
 
 import 'pages/teacher_profile_page.dart';
 
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'TeachMe',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const TeacherProfilePage(),
+      home: LoadingPage(),
     );
   }
 }
