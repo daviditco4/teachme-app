4106d7c36e5c0505a6f71bc57100a62f689ceceb
Matias <mdaneri@itba.edu.ar>
2022-05-14 17:50:50 -0300

    Se comenzó la implementacion de firebase. Funciona el registro, aunque sin redireccionamiento.

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,49 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:teachme_app/pages/MyClass_page.dart';
+import 'package:teachme_app/firebase_options.dart';
 import 'package:teachme_app/pages/auth_page.dart';
-import 'package:teachme_app/pages/notifications_page.dart';
-import 'package:teachme_app/pages/settings_page.dart';
-import 'package:teachme_app/pages/log_in_page.dart';
-import 'package:teachme_app/pages/loading_page.dart';
+import 'package:teachme_app/pages/splash_page.dart';
 
-import 'pages/lesson_search/teacher_profile_page.dart';
+const CHAT_TOPIC = 'public';
 
-void main() => runApp(const MyApp());
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TeachMe',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoadingPage(),
+    /*
+    const primaryColor = Color.fromARGB(255, 0, 42, 127);
+    const secondaryColor = Color.fromARGB(255, 95, 125, 226)
+    final defaultTextTheme = Typography.material2018().englishLike;
+*/
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (ctx, snapshot) {
+        return GestureDetector(
+          onTap: () {
+            final focusScope = FocusScope.of(ctx);
+            if (!focusScope.hasPrimaryFocus) focusScope.unfocus();
+          },
+          child: MaterialApp(
+              title: 'TeachMe-app',
+              theme: ThemeData(
+                primaryColor: Color.fromARGB(255, 0, 42, 127),
+                primaryColorDark: Color.fromARGB(255, 95, 125, 226),
+                accentColor: Color(0xff00adb5),
+                backgroundColor: Color(0xffeeeeee),
+              ),
+              home: snapshot.connectionState != ConnectionState.done
+                  ? SplashPage()
+                  : AuthPage()),
+        );
+      },
     );
   }
 }
