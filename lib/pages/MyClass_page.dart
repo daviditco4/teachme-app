428290ab0f34a0116d93a4a636cd3f59073e027d
AMattiussi <amattiussi@itba.edu.ar>
2022-05-15 19:22:13 -0300

    Se agrego la appBar a las pantallas restantes

--- lib/pages/MyClass_page.dart
+++ lib/pages/MyClass_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:teachme_app/constants/theme.dart';
 import 'package:teachme_app/widgets/bottom_nav_bar.dart';
 import 'package:teachme_app/widgets/card_class.dart';
+import '../widgets/other/tm_navigator.dart';
+import 'package:teachme_app/pages/notifications_page.dart';
+import 'package:teachme_app/pages/settings_page.dart';
 
 class MyClass extends StatefulWidget {
   const MyClass({Key? key}) : super(key: key);
@@ -16,6 +19,35 @@ class _MyClass extends State<MyClass> {
     return Scaffold(
         extendBodyBehindAppBar: true,
         backgroundColor: MyColors.background,
+        appBar: AppBar(
+          leading: const ImageIcon(
+            AssetImage("assets/images/teach_me_logo.png"),
+            color: MyColors.black,
+          ),
+          centerTitle: true,
+          title: const Text('Mis Clases',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 25,
+                fontWeight: FontWeight.w900,
+              )),
+          actions: [
+            IconButton(
+                icon: const Icon(Icons.settings, color: Colors.black),
+                onPressed: () =>
+                    TMNavigator.navigateTo(context, const SettingsPage())),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: IconButton(
+                icon: const Icon(Icons.notifications_none, color: Colors.black),
+                onPressed: () =>
+                    TMNavigator.navigateTo(context, const NotificationsPage()),
+              ),
+            ),
+          ],
+          backgroundColor: MyColors.background,
+          elevation: 0,
+        ),
         bottomNavigationBar: TMBottomNavigationBar(),
         body: SafeArea(
           child: SingleChildScrollView(
@@ -25,13 +57,13 @@ class _MyClass extends State<MyClass> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: const <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top: 5, bottom: 40),
-                    child: Text("Mis Clases",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.bold)),
-                  ),
+                  // Padding(
+                  //   padding: EdgeInsets.only(top: 5, bottom: 40),
+                  //   child: Text("Mis Clases",
+                  //       textAlign: TextAlign.center,
+                  //       style: TextStyle(
+                  //           fontSize: 30, fontWeight: FontWeight.bold)),
+                  // ),
                   Padding(
                     padding: EdgeInsets.only(top: 5, bottom: 40),
                     child:
--- lib/pages/lesson_search/teacher_profile_page.dart
+++ lib/pages/lesson_search/teacher_profile_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:teachme_app/widgets/bottom_nav_bar.dart';
-
+import 'package:teachme_app/constants/theme.dart';
 import '../../widgets/other/teacher_profile_overview.dart';
-import '../../widgets/other/top_bar.dart';
+import '../../widgets/other/tm_navigator.dart';
+import 'package:teachme_app/pages/notifications_page.dart';
+import 'package:teachme_app/pages/settings_page.dart';
 
 class TeacherProfilePage extends StatelessWidget {
   const TeacherProfilePage({Key? key}) : super(key: key);
@@ -10,8 +12,37 @@ class TeacherProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: MyColors.background,
       bottomNavigationBar: const TMBottomNavigationBar(),
-      appBar: TopBar(title: 'TeachMe', showSettings: true),
+      appBar: AppBar(
+          leading: const ImageIcon(
+            AssetImage("assets/images/teach_me_logo.png"),
+            color: MyColors.black,
+          ),
+          centerTitle: true,
+          title: const Text('Chat',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 25,
+                fontWeight: FontWeight.w900,
+              )),
+          actions: [
+            IconButton(
+                icon: const Icon(Icons.settings, color: Colors.black),
+                onPressed: () =>
+                    TMNavigator.navigateTo(context, const SettingsPage())),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: IconButton(
+                icon: const Icon(Icons.notifications_none, color: Colors.black),
+                onPressed: () =>
+                    TMNavigator.navigateTo(context, const NotificationsPage()),
+              ),
+            ),
+          ],
+          backgroundColor: MyColors.background,
+          elevation: 0,
+        ),
       body: Card(
         margin: const EdgeInsets.all(20.0),
         shape: RoundedRectangleBorder(
