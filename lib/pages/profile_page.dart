e65bb208d86c08617323c8d648223e6ab0981bd0
Matias <mdaneri@itba.edu.ar>
2022-06-03 15:36:03 -0300

    Profile
    Se corrigió el tema de que no muestra algunos datos del perfil. Si bien no anda todavía, el error está en la parte de guardado o retrieve(ver print)

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -24,178 +24,183 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        extendBodyBehindAppBar: true,
-        backgroundColor: MyColors.background,
-        bottomNavigationBar: const TMBottomNavigationBar(),
-        appBar: AppBar(
-          leading: const ImageIcon(
-            AssetImage("assets/images/teach_me_logo.png"),
-            color: MyColors.black,
-          ),
-          centerTitle: true,
-          title: const Text('Mi Perfil',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 25,
-                fontWeight: FontWeight.w900,
-              )),
-          actions: [
-            IconButton(
-                icon: const Icon(Icons.settings, color: Colors.black),
-                onPressed: () =>
-                    TMNavigator.navigateToPage(context, const SettingsPage())),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: IconButton(
-                icon: const Icon(Icons.notifications_none, color: Colors.black),
-                onPressed: () => TMNavigator.navigateToPage(
-                    context, const NotificationsPage()),
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.userChanges(),
+        builder: (_ , snap) {
+          print(snap.data);
+          return Scaffold(
+              extendBodyBehindAppBar: true,
+              backgroundColor: MyColors.background,
+              bottomNavigationBar: const TMBottomNavigationBar(),
+              appBar: AppBar(
+                leading: const ImageIcon(
+                  AssetImage("assets/images/teach_me_logo.png"),
+                  color: MyColors.black,
+                ),
+                centerTitle: true,
+                title: const Text('Mi Perfil',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.w900,
+                    )),
+                actions: [
+                  IconButton(
+                      icon: const Icon(Icons.settings, color: Colors.black),
+                      onPressed: () =>
+                          TMNavigator.navigateToPage(context, const SettingsPage())),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: IconButton(
+                      icon: const Icon(Icons.notifications_none, color: Colors.black),
+                      onPressed: () => TMNavigator.navigateToPage(
+                          context, const NotificationsPage()),
+                    ),
+                  ),
+                ],
+                backgroundColor: MyColors.background,
+                elevation: 0,
               ),
-            ),
-          ],
-          backgroundColor: MyColors.background,
-          elevation: 0,
-        ),
-        body: Stack(children: <Widget>[
-          SafeArea(
-            child: ListView(children: [
-              Padding(
-                padding:
-                    const EdgeInsets.only(left: 16.0, right: 16.0, top: 74.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Stack(children: <Widget>[
-                      Container(
-                        decoration: BoxDecoration(
-                          color: MyColors.cardClass,
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(30.0)),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.1),
-                              spreadRadius: 1,
-                              blurRadius: 7,
-                              offset: const Offset(
-                                  0, 3), // changes position of shadow
-                            ),
-                          ],
-                        ),
-                        child: Card(
-                            color: MyColors.cardClass,
-                            semanticContainer: true,
-                            clipBehavior: Clip.antiAliasWithSaveLayer,
-                            elevation: .0,
-                            shape: const RoundedRectangleBorder(
+              body: Stack(children: <Widget>[
+                SafeArea(
+                  child: ListView(children: [
+                    Padding(
+                      padding:
+                      const EdgeInsets.only(left: 16.0, right: 16.0, top: 74.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Stack(children: <Widget>[
+                            Container(
+                              decoration: BoxDecoration(
+                                color: MyColors.cardClass,
                                 borderRadius:
-                                    BorderRadius.all(Radius.circular(30.0))),
-                            child: Padding(
-                              padding: const EdgeInsets.only(
-                                  top: 85.0, bottom: 20.0),
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                    child: Column(
-                                      children: [
-                                        const SizedBox(height: 40.0),
-                                        Align(
-                                          child: Text(_getUsername(),
-                                              style: const TextStyle(
-                                                  color: MyColors.black,
-                                                  fontSize: 28.0,
-                                                  fontWeight: FontWeight.bold)),
-                                        ),
-                                        const SizedBox(height: 10.0),
-                                        const Divider(
-                                          height: 40.0,
-                                          thickness: 1.5,
-                                          indent: 32.0,
-                                          endIndent: 32.0,
-                                        ),
-                                        const Padding(
-                                          padding: EdgeInsets.only(
-                                              left: 32.0, right: 32.0),
-                                          child: Align(
-                                            child: Text(
-                                                "Hola!!! Actualmente estoy cursando el tercer año de ingeniería mecánica",
-                                                textAlign: TextAlign.center,
-                                                style: TextStyle(
-                                                    color: MyColors.black,
-                                                    fontSize: 17.0,
-                                                    fontWeight:
-                                                        FontWeight.w200)),
-                                          ),
-                                        ),
-                                        const SizedBox(height: 25.0),
-                                        Padding(
-                                          padding: const EdgeInsets.only(
-                                              right: 25.0, left: 25.0),
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: const [
-                                              Text(
-                                                "Calificación",
-                                                style: TextStyle(
-                                                    fontWeight: FontWeight.bold,
-                                                    fontSize: 20.0,
-                                                    color: MyColors.black),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                        const SizedBox(height: 100.0),
-                                        Padding(
-                                          padding: const EdgeInsets.only(
-                                              right: 25.0, left: 25.0),
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: const [
-                                              Text(
-                                                "Comentarios",
-                                                style: TextStyle(
-                                                    fontWeight: FontWeight.bold,
-                                                    fontSize: 20.0,
-                                                    color: MyColors.black),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                        const SizedBox(
-                                          height: 200,
-                                          // child: GridView.count(),
-                                        )
-                                      ],
-                                    ),
+                                const BorderRadius.all(Radius.circular(30.0)),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.1),
+                                    spreadRadius: 1,
+                                    blurRadius: 7,
+                                    offset: const Offset(
+                                        0, 3), // changes position of shadow
                                   ),
                                 ],
                               ),
-                            )),
-                      ),
-                      FractionalTranslation(
-                          translation: const Offset(0.0, -0.5),
-                          child: Align(
-                            child: CircleAvatar(
-                              backgroundColor: MyColors.white,
-                              backgroundImage: _getUserImage(),
-                              radius: 65.0,
-                              // maxRadius: 200.0,
+                              child: Card(
+                                  color: MyColors.cardClass,
+                                  semanticContainer: true,
+                                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                                  elevation: .0,
+                                  shape: const RoundedRectangleBorder(
+                                      borderRadius:
+                                      BorderRadius.all(Radius.circular(30.0))),
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 85.0, bottom: 20.0),
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                          child: Column(
+                                            children: [
+                                              const SizedBox(height: 40.0),
+                                              Align(
+                                                child: Text(_getUsername(),
+                                                    style: const TextStyle(
+                                                        color: MyColors.black,
+                                                        fontSize: 28.0,
+                                                        fontWeight: FontWeight.bold)),
+                                              ),
+                                              const SizedBox(height: 10.0),
+                                              const Divider(
+                                                height: 40.0,
+                                                thickness: 1.5,
+                                                indent: 32.0,
+                                                endIndent: 32.0,
+                                              ),
+                                              const Padding(
+                                                padding: EdgeInsets.only(
+                                                    left: 32.0, right: 32.0),
+                                                child: Align(
+                                                  child: Text(
+                                                      "Hola!!! Actualmente estoy cursando el tercer año de ingeniería mecánica",
+                                                      textAlign: TextAlign.center,
+                                                      style: TextStyle(
+                                                          color: MyColors.black,
+                                                          fontSize: 17.0,
+                                                          fontWeight:
+                                                          FontWeight.w200)),
+                                                ),
+                                              ),
+                                              const SizedBox(height: 25.0),
+                                              Padding(
+                                                padding: const EdgeInsets.only(
+                                                    right: 25.0, left: 25.0),
+                                                child: Row(
+                                                  mainAxisAlignment:
+                                                  MainAxisAlignment.spaceBetween,
+                                                  children: const [
+                                                    Text(
+                                                      "Calificación",
+                                                      style: TextStyle(
+                                                          fontWeight: FontWeight.bold,
+                                                          fontSize: 20.0,
+                                                          color: MyColors.black),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                              const SizedBox(height: 100.0),
+                                              Padding(
+                                                padding: const EdgeInsets.only(
+                                                    right: 25.0, left: 25.0),
+                                                child: Row(
+                                                  mainAxisAlignment:
+                                                  MainAxisAlignment.spaceBetween,
+                                                  children: const [
+                                                    Text(
+                                                      "Comentarios",
+                                                      style: TextStyle(
+                                                          fontWeight: FontWeight.bold,
+                                                          fontSize: 20.0,
+                                                          color: MyColors.black),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                              const SizedBox(
+                                                height: 200,
+                                                // child: GridView.count(),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  )),
                             ),
-                            alignment: const FractionalOffset(0.5, 0.0),
-                          ))
-                    ]),
-                  ],
-                ),
-              ),
-            ]),
-          )
-        ]));
+                            FractionalTranslation(
+                                translation: const Offset(0.0, -0.5),
+                                child: Align(
+                                  child: CircleAvatar(
+                                    backgroundColor: MyColors.white,
+                                    backgroundImage: _getUserImage(),
+                                    radius: 65.0,
+                                    // maxRadius: 200.0,
+                                  ),
+                                  alignment: const FractionalOffset(0.5, 0.0),
+                                ))
+                          ]),
+                        ],
+                      ),
+                    ),
+                  ]),
+                )
+              ]));
+    });
   }
 
   String _getUsername() {
-    String? username = firebaseAuth.currentUser!.displayName;
+    String? username = firebaseAuth.currentUser!.email;
     return username ?? "ERROR";
   }
 
